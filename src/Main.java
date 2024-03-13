138b30ccb2c883e46f7205ab36a7fe09cfee00f6
Kseniakrivolutskaya <kraksinia@yandex.ru>
2024-03-13 12:51:25 +0300

    FirstCommit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Cat("Лео"));
+        animals.add(new Dog("Лулу"));
+        animals.add(new Bird("Чирик"));
+        animals.add(new Fish("Рыб"));
+
+        for (Animal a: animals) a.toGo();
+        System.out.println();
+
+        for (Animal a: animals) a.fly();
+        System.out.println();
+
+        for (Animal a: animals) a.swim();
+        System.out.println();
+    }
+}
